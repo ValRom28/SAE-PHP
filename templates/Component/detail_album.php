b437d0c712e65c2528745f6b74b6fd80154ed7e2
HugoSAINSON <hugo.sainson@etu.univ-orleans.fr>
2024-02-13 12:29:57 +0100

    correction

--- templates/Component/detail_album.php
+++ templates/Component/detail_album.php
@@ -10,7 +10,7 @@ if($album) {
         $pdo = new \PDO('sqlite:Data/db.sqlite');
         $request = new Request($pdo);
         if ($request->isAlbumInPlaylist($albumId, $idUtilisateur)) {
-            echo "<form action='/index.php?action=supprimer_playlist' method='post'>";
+            echo "<form action='/index.php?action=supprimer_playlist' method='post'class='favForm'>";
             echo "<h2>".$album[0]['nomAlbum']."</h2>";
             echo "<input type='hidden' name='album_id' value='".$albumId."'>";
             echo "<button type='submit' class='favImg'><img src='templates/static/images/favFull.png'></button>";
@@ -27,7 +27,7 @@ if($album) {
     else{
         echo "<h2>".$album[0]['nomAlbum']."</h2>";
     }
-    echo "<p>".$album[0]['descriptionAlbum']."</p>";
+    echo "<p>".$album[0]['description']."</p>";
     echo "<p>Année de sortie: ".$album[0]['anneeSortie']."</p></div>";
     echo "<img src='Data/images/".$album[0]['lienImage']."' alt='".$album[0]['nomAlbum']."' class='imgGroupe'/>";
 } else {
