32365cdcd3e658c13b6f66e5e8eeb281481350cd
ValRom <valentin.romanet@etu.univ-orleans.fr>
2024-02-18 22:09:30 +0100

    doc : documentation pour les classes du provider

--- Classes/Provider/DataLoaderInterface.php
+++ Classes/Provider/DataLoaderInterface.php
@@ -1,7 +1,17 @@
 <?php
 namespace Provider;
 
+/**
+ * Interface pour les classes de chargement de données
+ * 
+ */
 interface DataLoaderInterface {
+
+    /**
+     * Récupère les données
+     * 
+     * @return array
+     */
     public function getData(): array;
 }
 
--- Classes/Provider/DataLoaderJson.php
+++ Classes/Provider/DataLoaderJson.php
@@ -1,9 +1,18 @@
 <?php
 namespace Provider;
 
+/**
+ * Classe pour charger des données depuis un fichier JSON
+ * 
+ */
 final class DataLoaderJson implements DataLoaderInterface {
     private $data;
 
+    /**
+     * Constructeur de la classe
+     * 
+     * @param string $source
+     */
     public function __construct(string $source) {
         $content = file_get_contents($source);
         $this->data = json_decode($content, true);
@@ -13,6 +22,11 @@ final class DataLoaderJson implements DataLoaderInterface {
         }
     }
 
+    /**
+     * Retourne les données
+     * 
+     * @return array
+     */
     public function getData(): array {
         return $this->data;
     }
